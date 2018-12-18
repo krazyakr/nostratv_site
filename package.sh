ee54352abec66ea33be5da377f7b2449b8248313
Rafael Torres <rtorres@smithmicro.com>
2018-12-18 16:26:53 +0000

    Remove cp command option that does not work on MACOS X

--- package.sh
+++ package.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 rm -r build/
 mkdir build && mkdir build/dev && mkdir build/release
-cp --verbose *.sh build/dev && cp --verbose *.sh build/release
+cp *.sh build/dev && cp *.sh build/release
 rm build/dev/package.sh && rm build/release/package.sh
-cp -r --verbose src/ build/dev/src && cp -r --verbose src/ build/release/src
+cp -r src/ build/dev/src && cp -r src/ build/release/src
 rm -r build/dev/src/node_modules && rm -r build/release/src/node_modules
-cp --verbose dev-docker-compose.yml build/dev/docker-compose.yml
-cp --verbose docker-compose.yml build/release/docker-compose.yml
-cp --verbose Dockerfile build/dev/Dockerfile
-cp --verbose Dockerfile build/release/Dockerfile
+cp dev-docker-compose.yml build/dev/docker-compose.yml
+cp docker-compose.yml build/release/docker-compose.yml
+cp Dockerfile build/dev/Dockerfile
+cp Dockerfile build/release/Dockerfile
