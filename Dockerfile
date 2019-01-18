3d4a46218bb94fef4fead33fd65defecd480744e
krazyakr <krazyakr@gmail.com>
2019-01-18 14:40:46 +0000

    Device commands (#8)
    * add device router stub
    
    * More device api stubs
    
    * Added device controller
    
    * Added support for getGenericFile
    
    * Updated .gitignore
    
    * removed docker compose default file
    
    * Added support to device commands and device files
    
    * change port to 80

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
 FROM node:alpine
 
-WORKDIR /usr/app
+WORKDIR /usr/app/nostratv_site
 
 RUN apk update && apk upgrade && \
     apk add --no-cache bash git openssh
 
 RUN cd /tmp/ && \
     git clone https://github.com/krazyakr/nostratv_site.git && \
-    cp -r /tmp/nostratv_site/src/* /usr/app/ && \
-    cd /usr/app
+    cp -r /tmp/nostratv_site/src/* /usr/app/nostratv_site/ && \
+    cd /usr/app/nostratv_site
 
 RUN npm install
 
---
+++ src/routes/api/device.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+// Required controller modules
+var controller = require('../../controllers/api/device');
+
+// Routes
+
+// Default handler
+router.get('/', function (req, res, next) {
+  res.statusCode = 501;
+  res.json({ "Error": "Not Implemented" });
+});
+
+// Get Generic File
+router.get('/file/:fileID', controller.getGenericFile);
+
+// Get Device commands
+router.get('/:deviceID/:secret/commands/:version', controller.getDeviceCommands);
+
+// Get Device file
+router.get('/:deviceID/:secret/file/:fileID', controller.getDeviceFile);
+
+module.exports = router;
